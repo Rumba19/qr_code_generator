11a638ab2fb96fd1c8c0d206d4f0d27737cbd5a6
Rumba19 <brumba30721@ucumberlands.edu>
2025-05-17 13:48:40 -0700

    qr generator

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -1,18 +1,13 @@
 import qrcode
 
 def generate_qr_code(url):
-    """
-    Generates a QR Code from the provided URL and saves it as an image file.
-    
-    Args:
-        url (str): The URL to be converted into a QR code.
-    """
+
     # Generate the QR Code
     qr = qrcode.QRCode(
-        version=1,  # controls the size of the QR code
+        version=1,   
         error_correction=qrcode.constants.ERROR_CORRECT_L,  # level of error correction
-        box_size=10,  # size of each box in the QR code grid
-        border=4,  # thickness of the border
+        box_size=10,   
+        border=4,   
     )
     
     qr.add_data(url)
@@ -25,8 +20,7 @@ def generate_qr_code(url):
     img.save('qrcode.png')
 
     print("QR Code generated and saved as 'qrcode.png'")
-
-# Example usage
+ 
 if __name__ == "__main__":
     url = input("Enter the URL to generate a QR Code: ")
     generate_qr_code(url)
